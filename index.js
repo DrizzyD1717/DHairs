0dfb041ecff248db0cbcdd08a80514fece2a0a1c
Olulana Oluwadarimisire <82449711+DrizzyD1717@users.noreply.github.com>
2024-06-06 16:20:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ mobileMenuBrown.addEventListener("click", () => {
 openMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("hide");
 });
+
+// jump in effect below
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle(
+        "animate__fadeInUpBig",
+        entry.isIntersecting
+      );
+      console.log(entry);
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: "0" }
+);
+
+const cards = document.querySelectorAll(".addEffect");
+console.log(cards);
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
